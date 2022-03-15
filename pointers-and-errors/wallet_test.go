7582722878fe7ea25aa13c289a0f92568d028d53
Steven Noorbergen <noorbergen@ccpgames.com>
2022-03-15 14:55:18 +0100

    Create Withdraw method

--- pointers-and-errors/wallet_test.go
+++ pointers-and-errors/wallet_test.go
@@ -4,14 +4,31 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 
-	wallet := Wallet{}
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(10))
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
-	want := Bitcoin(10)
+		got := wallet.Balance()
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
