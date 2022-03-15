fb1ac80338f542d03567aadfc8c4126f74d59060
Steven Noorbergen <noorbergen@ccpgames.com>
2022-03-15 14:19:37 +0100

    Add Perimiter function to the shapes package

---
+++ structs-methods-and-interfaces/shapes.go
@@ -0,0 +1,9 @@
+package shapes
+
+func Area(width float64, height float64) float64 {
+	return width * height
+}
+
+func Perimeter(width float64, height float64) float64 {
+	return 2 * (width + height)
+}
