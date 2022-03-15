0186ae9d4101b709ba570095af34109a8c4e13d2
Steven Noorbergen <noorbergen@ccpgames.com>
2022-03-15 13:00:51 +0100

    Add French and Spanish language support

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -3,14 +3,24 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const frenchHelloPrefix = "Bonjour, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
+	}
+	if language == "French" {
+		return frenchHelloPrefix + name
+	}
+
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "English"))
 }
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -10,14 +10,15 @@ func TestHello(t *testing.T) {
 		}
 	}
 
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
-	t.Run("empty string defaults to 'World'", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, World"
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
