0ee693b4e1e5a45a7c79bd5378689e1870446aa9
Steven Noorbergen <noorbergen@ccpgames.com>
2022-03-15 14:23:40 +0100

    Add triangle shape support and introduce table driven tests

--- structs-methods-and-interfaces/shapes.go
+++ structs-methods-and-interfaces/shapes.go
@@ -14,12 +14,21 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
 
 func Perimeter(r Rectangle) float64 {
--- structs-methods-and-interfaces/shapes_test.go
+++ structs-methods-and-interfaces/shapes_test.go
@@ -16,22 +16,20 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Circle{10}, 314.1592653589793},
+		{Rectangle{12, 6}, 72.0},
+		{Triangle{12, 6}, 36.0},
 	}
 
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
-
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		checkArea(t, rectangle, 72.0)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
 
 }
