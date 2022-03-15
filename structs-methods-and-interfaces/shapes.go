94bb56f3c68d35dbfce133d27d23327ca4ea0173
Steven Noorbergen <noorbergen@ccpgames.com>
2022-03-15 14:20:22 +0100

    Refactor using structs

--- structs-methods-and-interfaces/shapes.go
+++ structs-methods-and-interfaces/shapes.go
@@ -1,9 +1,14 @@
 package shapes
 
-func Area(width float64, height float64) float64 {
-	return width * height
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+func Area(r Rectangle) float64 {
+	return r.Width * r.Height
+}
+
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.Width + r.Height)
 }
