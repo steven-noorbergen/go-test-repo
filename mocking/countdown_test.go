9a97af59fb5bbe6d2a401723f490367cd12d2ffe
Steven Noorbergen <noorbergen@ccpgames.com>
2022-03-15 22:44:18 +0100

    Create an observer to record the order in which pauses and prints are issued

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -1,35 +1,44 @@
 package countdown
 
 import (
-	"bytes"
+	"reflect"
 	"testing"
 )
 
-type ObservableSleeper struct {
-	Calls int
+type ObservableCountdownOperations struct {
+	Calls []string
 }
 
-func (s *ObservableSleeper) Sleep() {
-	s.Calls++
+func (s *ObservableCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
 }
+
+func (s *ObservableCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	observableSleeper := &ObservableSleeper{}
+	t.Run("sleep before every print", func(t *testing.T) {
+		observableSleepPrinter := &ObservableCountdownOperations{}
+		Countdown(observableSleepPrinter, observableSleepPrinter)
 
-	Countdown(buffer, observableSleeper)
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
 
-	got := buffer.String()
-	want := `3
-2
-1
-Go!`
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
-
-	if observableSleeper.Calls != 4 {
-		t.Errorf("not enough calls to sleeper, want 4 got %d", observableSleeper.Calls)
-	}
+		if !reflect.DeepEqual(want, observableSleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, observableSleepPrinter.Calls)
+		}
+	})
 }
