c9b117462c61660611c96794e95b4ab12aa15e0b
Steven Noorbergen <noorbergen@ccpgames.com>
2022-03-15 15:11:09 +0100

    Clean up Add method test

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -21,16 +21,24 @@ func TestSearch(t *testing.T) {
 
 func TestAdd(t *testing.T) {
 	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
+	word := "test"
+	definition := "this is just a test"
 
-	want := "this is just a test"
-	got, err := dictionary.Search("test")
+	dictionary.Add(word, definition)
+
+	assertDefinition(t, dictionary, word, definition)
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
 	if err != nil {
 		t.Fatal("should find added word:", err)
 	}
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	if definition != got {
+		t.Errorf("got %q want %q", got, definition)
 	}
 }
 
